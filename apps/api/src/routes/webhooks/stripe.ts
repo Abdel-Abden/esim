f18b9355dadf9214f2c27f8344de66ffb4670cc1
Alexis-HILLION <alexishillion@hotmail.fr>
2026-02-24 10:46:21 +0100

    fix: adding missing import

--- apps/api/src/routes/webhooks/stripe.ts
+++ apps/api/src/routes/webhooks/stripe.ts
@@ -1,5 +1,5 @@
 import { Hono } from 'hono';
-import { confirmEsim } from '../../db/queries/esims.js';
+import { confirmEsim, releaseEsim } from '../../db/queries/esims.js';
 import {
   getOrderByPaymentIntentId,
   updateOrderStatus
