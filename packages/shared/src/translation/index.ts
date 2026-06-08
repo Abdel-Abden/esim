7b7ca0607889431dd0391c009be452a430d1eab2
Alexis-HILLION <alexishillion@hotmail.fr>
2026-06-08 14:36:59 +0200

    fix: correcting import on json file for translation

--- packages/shared/src/translation/index.ts
+++ packages/shared/src/translation/index.ts
@@ -1,11 +1,15 @@
 import { DEFAULT_LANG } from '../constants.js';
-import ar from './ar.json';
-import de from './de.json';
-import en from './en.json';
-import fr from './fr.json';
+import ar from './ar.json' with { type: 'json' };
+import de from './de.json' with { type: 'json' };
+import en from './en.json' with { type: 'json' };
+import fr from './fr.json' with { type: 'json' };
 
-export const esimsTranslations = { fr, en, de, ar } as const;
+export const esimsTranslations: Record<string, Translation> = { fr, en, de, ar } as const;
 type GeoLang = keyof typeof esimsTranslations;
+type Translation = {
+  region: Record<string, string>;
+  country: Record<string, string>;
+}
 
 export function getCountryName(code: string, lang?: string): string {
   const l = (lang && lang in esimsTranslations ? lang : DEFAULT_LANG) as GeoLang;
